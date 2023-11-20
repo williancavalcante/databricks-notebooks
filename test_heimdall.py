3da9d52d48f6e5793c23d939509d2df7dfddf480
williancavalcante <williandesousacavalcante@gmail.com>
2023-11-20 18:26:38 -0300

    add teste

--- test_heimdall.py
+++ test_heimdall.py
@@ -1,6 +1,8 @@
 from databricks.connect.session import DatabricksSession as SparkSession
 from databricks.sdk.core import Config
 from pyspark.testing.utils import assertDataFrameEqual
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 from heimdall import StorageContext
 import unittest
 
@@ -86,3 +88,20 @@ def test_dataframe_from_table():
 
         # Assert
         assertDataFrameEqual(df, expected_df)
+
+def test_dataframe_equality():
+        data = [("Alice", 1), ("Bob", 2)]
+        df1 = spark.createDataFrame(data, ["name", "id"])
+        df2 = spark.createDataFrame(data, ["name", "id"])
+
+        assertDataFrameEqual(df1, df2)
+
+def test_dataframe_filtering():
+    data = [("Alice", 1), ("Bob", 2), ("Charlie", 3)]
+    df = spark.createDataFrame(data, ["name", "id"])
+    filtered_df = df.filter(df["id"] > 1)
+
+    expected_data = [("Bob", 2), ("Charlie", 3)]
+    expected_df = spark.createDataFrame(expected_data, ["name", "id"])
+
+    assertDataFrameEqual(filtered_df, expected_df)
