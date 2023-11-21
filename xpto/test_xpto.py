cd84884293ca0007aa534fbaf4efbb6e62ac485a
williancavalcante <williandesousacavalcante@gmail.com>
2023-11-20 21:58:12 -0300

    teste xpto

--- xpto/test_xpto.py
+++ xpto/test_xpto.py
@@ -34,7 +34,6 @@ def test_dataframe_filtering():
     data = [("Alice", 1), ("Bob", 2), ("Charlie", 3)]
     df = spark.createDataFrame(data, ["name", "id"])
     filtered_df = df.filter(df["id"] > 1)
-    #ola
     expected_data = [("Bob", 2), ("Charlie", 3)]
     expected_df = spark.createDataFrame(expected_data, ["name", "id"])
 
